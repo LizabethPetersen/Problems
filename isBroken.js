7c12084e4090f426169969cd9a329d21a81a9b21
merittrompeter <mktrompeter@gmail.com>
2017-12-01 11:29:49 -0800

    Init

---
+++ isBroken.js
@@ -0,0 +1,31 @@
+// Create cash drawer to give change from
+const cashValues = [[2000, 4], [1000, 3], [500, 6], [100,16], [25, 42], [10, 26], [5, 57], [1, 87]];
+const cashAvailable = new Map(cashValues);
+
+// Create language lookup for denomination - used in returning natural languge answer
+const translateCash = {
+    2000: 'twenties',
+    1000: 'tens',
+    500: 'fives',
+    100: 'ones',
+    25: 'quarters',
+    10: 'dimes',
+    5: 'nickles',
+    1: 'pennies'
+};
+
+const makeChange = function(cost, paid) {    
+    change = {};
+    cashAvailable.forEach((available, denomination) => {
+        let changeDue = (paid - cost) * 100;
+        const amount = (available > 0) ? Math.floor(changeDue / denomination) : 0;
+        if (amount > 0) {
+            const currencyType = translateCash[denomination];
+            change[currencyType] = amount;
+            cashAvailable.set(denomination, (available - amount));
+            changeDue = changeDue - (amount * denomination);
+        }
+    });
+}(5.67, 10);
+
+console.log(change);
